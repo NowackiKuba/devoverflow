635f5ce6e7a19063b998c4248f3340e9e2f6c1d0
NowackiKuba <chujwdupechujowiichuj@gmail.com>
2023-10-05 13:37:23 +0200

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,28 @@
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
+import { Metadata } from 'next';
 import React from 'react';
+import { Inter, Space_Grotesk } from 'next/font/google';
 
-export const metadata = {
-  title: 'Next.js 13 with Clerk',
+export const metadata: Metadata = {
+  title: 'Devflow',
+  description:
+    'A community driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.',
+  icons: {
+    icon: '/assets/images/site-logo.svg',
+  },
 };
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk',
+});
 
 export default function RootLayout({
   children,
@@ -14,7 +32,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
