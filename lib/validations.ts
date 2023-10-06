62a234da1054a6f9ff02f8e2ba43c7f16c654a40
NowackiKuba <chujwdupechujowiichuj@gmail.com>
2023-10-06 17:22:07 +0200

    Implement whole ask question ui

---
+++ lib/validations.ts
@@ -0,0 +1,10 @@
+import * as z from 'zod';
+
+export const questionsSchema = z.object({
+  title: z
+    .string()
+    .min(5, { message: 'Title should be minimum 5 characters' })
+    .max(130),
+  explanation: z.string().min(100),
+  tags: z.array(z.string().min(1).max(15)).min(1).max(3),
+});
