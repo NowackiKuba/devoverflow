ec0dfa01a9d9ab0fdb8fceb4505024f8ea4994b0
NowackiKuba <chujwdupechujowiichuj@gmail.com>
2023-10-10 00:42:21 +0200

    fixed filtering UI  in the homepage

--- components/shared/home/HomeFilters.tsx
+++ components/shared/home/HomeFilters.tsx
@@ -1,32 +1,36 @@
 'use client';
 import { HomePageFilters } from '@/constants/filters';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { formUrlQuery, removeKeysFromQuery } from '@/lib/utils';
-import router from 'next/router';
+import { formUrlQuery } from '@/lib/utils';
 
 const HomeFilters = () => {
   const searchParams = useSearchParams();
-  const [selectedFilter, setSelectedFilter] = useState<string>('');
-  const active = selectedFilter || '';
+
   searchParams.get('filter');
+  const [active, setActive] = useState<string>('');
   const router = useRouter();
 
-  useEffect(() => {
-    const delayDebounceFn = setTimeout(() => {
-      if (selectedFilter) {
-        const newUrl = formUrlQuery({
-          params: searchParams.toString(),
-          key: 'filter',
-          value: selectedFilter,
-        });
-        router.push(newUrl, { scroll: false });
-      }
-    }, 500);
-
-    return () => clearTimeout(delayDebounceFn);
-  }, [selectedFilter, router, searchParams]);
+  const handleTypeClick = (item: string) => {
+    if (active === item) {
+      setActive('');
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: 'filter',
+        value: null,
+      });
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive(item);
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: 'filter',
+        value: item.toLowerCase(),
+      });
+      router.push(newUrl, { scroll: false });
+    }
+  };
   return (
     <div className='mt-10 flex-wrap gap-3 md:flex'>
       {HomePageFilters.map((filter) => (
@@ -37,7 +41,7 @@ const HomeFilters = () => {
               : 'bg-light-800 text-light-500 hover:bg-light-700 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-400'
           }`}
           key={filter.value}
-          onClick={() => setSelectedFilter(filter.value)}
+          onClick={() => handleTypeClick(filter.value)}
         >
           {filter.name}
         </Button>
