7bfbb1caa2ee1125fceef7a100d3fcd0181f4f73
NowackiKuba <chujwdupechujowiichuj@gmail.com>
2023-10-09 18:11:49 +0200

    First version of the edit profile page

--- lib/validations.ts
+++ lib/validations.ts
@@ -12,3 +12,20 @@ export const questionsSchema = z.object({
 export const AnswerSchema = z.object({
   answer: z.string().min(100),
 });
+
+export const userSchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: 'Name should be at least 3 characters' })
+    .max(20),
+  username: z
+    .string()
+    .min(5, { message: 'Username should be at least 5 characters' })
+    .max(32),
+  portfolioLink: z.string().url(),
+  location: z.string().max(30),
+  bio: z
+    .string()
+    .min(30, { message: 'Bio should be at least 30 characters' })
+    .max(150),
+});
