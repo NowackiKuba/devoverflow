4c7f551b142a298ba28d36d47abc5841dc99fa50
NowackiKuba <chujwdupechujowiichuj@gmail.com>
2023-10-11 19:33:50 +0200

    Implement loading for home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -9,6 +9,7 @@ import { HomePageFilters } from '@/constants/filters';
 import { getQuestions } from '@/lib/actions/question.actions';
 import { SearchParamsProps } from '@/types';
 import Link from 'next/link';
+import Loading from './loading';
 
 export default async function Home({ searchParams }: SearchParamsProps) {
   const result = await getQuestions({
@@ -16,8 +17,6 @@ export default async function Home({ searchParams }: SearchParamsProps) {
     filter: searchParams.filter,
     page: searchParams.page ? +searchParams.page : 1,
   });
-
-  // TODO Create Fetch and action for recommendation system
 
   return (
     <>
